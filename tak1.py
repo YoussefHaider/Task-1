157ef8a8757dbb699027de3025eb66295bd174be
Youssef Nagah Ahmed Awaad Haider <21-101139@students.eui.edu.eg>
2024-02-21 12:49:02 +0200

    comment

--- tak1.py
+++ tak1.py
@@ -1,10 +1,12 @@
 names=[]
 id1=[]
+GPA=[]
 for _ in range(3):
     temp=input("enter ur name: ")
     names.append(temp)
     temp2=input("enter ur id: ")
     id1.append(temp2)
+    temp3=input("enter ur GPA: ")
    
 for _ in range(3):
-    print(names[_] + id1[_])
+    print(names[_] + id1[_] + GPA[_])
