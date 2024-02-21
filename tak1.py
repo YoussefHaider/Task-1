43cc8d66d2e128bd9d6b9b425d1e7d289202dcd8
sir3bdo <112068649+sir3bdo@users.noreply.github.com>
2024-02-21 12:41:14 +0200

    Create tak1.py

---
+++ tak1.py
@@ -0,0 +1,10 @@
+names=[]
+id1=[]
+for _ in range(3):
+    temp=input("enter ur name: ")
+    names.append(temp)
+    temp2=input("enter ur id: ")
+    id1.append(temp2)
+   
+for _ in range(3):
+    print(names[_] + id1[_])
